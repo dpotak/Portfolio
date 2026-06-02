039b6e5360fab81e544cb3cbe373ebb9b54bd0ce
dpotak <darjapotak248@gmail.com>
2026-06-02 21:40:00 +0300

    Add new changes

--- js_portfolio/portfolio.js
+++ js_portfolio/portfolio.js
@@ -25,6 +25,8 @@ contacts_title:"Contacts:",
 
 powershell_name_1:"PowerShell: Installed and Settings network",
 
+programming_lang:"Programmering languages",
+
 p_blog_1: "It's my blog.",
 p_blog_2: "At the moment it is still under development.",
 
@@ -199,6 +201,7 @@ exp_title:"Опыт:",
 contacts_title:"Контакты:",
 
 powershell_name_1:"PowerShell: Установка ​​и настройка сети",
+programming_lang:"Языки программирование",
 
 p_blog_1: "Это мой блог",
 p_blog_2: "На данный момент он пока еще находиться в разработке",
@@ -373,6 +376,7 @@ exp_title:"Töökogemus:",
 contacts_title:"Kontakt:",
 
 powershell_name_1:"PowerShell: võrgu installimine ja seadistamine",
+programming_lang:"Programmeerimiskeeled",
 
 p_blog_1: "See on minu blogi.",
 p_blog_2: "Hetkel on see veel arendusjärgus.",
