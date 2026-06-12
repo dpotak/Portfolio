c83c5c10ee0fd928a6a82c8ed926c3bd4904711f
dpotak <darjapotak248@gmail.com>
2026-06-12 22:08:31 +0300

    Add new changes

--- js_portfolio/portfolio.js
+++ js_portfolio/portfolio.js
@@ -563,3 +563,27 @@ el.innerText=translations[lang][el.dataset.i18n];
 function toggleDropdown() {
     document.getElementById("dropdown").classList.toggle("show");
 }
+
+const projects = document.querySelectorAll(
+  '.kvadrat_1_progetcs, .kvadrat_2_progetcs, .kvadrat_3_progetcs, .kvadrat_4_progetcs, .kvadrat_5_progetcs'
+);
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.15
+});
+
+projects.forEach(p => observer.observe(p));
+
+function toggleSkill(id){
+
+const block=document.getElementById(id);
+
+block.classList.toggle("active");
+
+}
