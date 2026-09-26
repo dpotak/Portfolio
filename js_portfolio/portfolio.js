0be1c00f18d21d1ecfd999bfa454fb81da1673bf
dpotak <darjapotak248@gmail.com>
2026-09-26 15:45:59 +0300

    add new changes

--- js_portfolio/portfolio.js
+++ js_portfolio/portfolio.js
@@ -73,8 +73,9 @@ sliderCrazy_practice: "Slider Crazy (practice)",
 Manga_Chan_ComingSoon: "Manga--Chan--Anime--Chan (coming soon)",
 TrackerCleaning_comingSoon: "Tracker Cleaning (coming soon)",
 TodoApp_NEWOLD_Version: "Todo App (New/Old Version)",
-JuniorSpecialist_Level4: "",
-ITTechician_Level4: "",
+JuniorSpecialist_Level4: "Junior IT Systems - Level 4",
+ITTechician_Level4: "IT-Techician - Level 4",
+CERT_EST_LANG_A2: "CERTIFICATE ESTONIAN LANGUAGE A2 (official) 60%:",
 
 /*  */
 
